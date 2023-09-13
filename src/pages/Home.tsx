66caee2086cc32a887985e1cc7d7327b6e82ba0d
benjv_tdh <cortesbenja@gmail.com>
2023-09-12 22:57:41 -0300

    add toggle for no entregados

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,6 +10,7 @@ import {
   IonRouterLink,
   IonRow,
   IonTitle,
+  IonToggle,
   IonToolbar,
 } from "@ionic/react";
 
@@ -17,10 +18,15 @@ import "./Home.css";
 import Paquete from "../components/Paquete";
 import Repartidor from "../components/Repartidor";
 import { PaquetesContext } from "../interfaces/paquetes";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 const Home: React.FC = () => {
   const { paquetesList, repartidoresList } = useContext(PaquetesContext);
+  const [ordenarNoEntregados, setOrdenarNoEntrados] = useState(false);
+
+  const paquetes = ordenarNoEntregados
+    ? paquetesList.slice().filter((paq) => !paq.enviado)
+    : paquetesList;
   return (
     <IonPage>
       <IonHeader>
@@ -33,14 +39,19 @@ const Home: React.FC = () => {
           <IonListHeader>
             <IonLabel>Listado de Paquetes</IonLabel>
           </IonListHeader>
-          {paquetesList.map((paq) => (
+          {paquetes.map((paq) => (
             <Paquete paquete={paq} key={paq.id} />
           ))}
-          <IonRow>
-            <IonCol className="ion-text-center">
-              <IonRouterLink routerLink="/agregar-paquete">
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="6">
+              {/* <IonRouterLink routerLink="/agregar-paquete">
                 <IonButton shape="round">Agregar Paquete</IonButton>
-              </IonRouterLink>
+              </IonRouterLink> */}
+              <IonToggle
+                onIonChange={() => setOrdenarNoEntrados((ordenar) => !ordenar)}
+              >
+                No entregados
+              </IonToggle>
             </IonCol>
           </IonRow>
         </IonList>
